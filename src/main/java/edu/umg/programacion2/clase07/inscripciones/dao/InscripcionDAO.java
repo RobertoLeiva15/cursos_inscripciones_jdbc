7f0ef43f9c0e6f0f32c4f42c53c7c2a6431d45f2
Jose Roberto Leiva Salazar <jleivas@miumg.edu.gt>
2026-09-04 14:23:24 -0600

    feat: se implemento listarCursosDeEstudiante con JOIN de 3 tablas

--- src/main/java/edu/umg/programacion2/clase07/inscripciones/dao/InscripcionDAO.java
+++ src/main/java/edu/umg/programacion2/clase07/inscripciones/dao/InscripcionDAO.java
@@ -95,8 +95,27 @@ public class InscripcionDAO {
      */
     public List<Curso> listarCursosDeEstudiante(String carnet) throws SQLException {
         List<Curso> resultado = new ArrayList<>();
-        // TODO: completar (ver pista del JOIN de 3 tablas arriba).
+        
+        String sql = "SELECT c.id, c.nombre, c.creditos "
+                + "FROM inscripciones i "
+                + "JOIN cursos c ON i.curso_id = c.id "
+                + "JOIN estudiantes e ON i.estudiante_id = e.id "
+                + "WHERE e.carnet = ?";
 
+     try (Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+          PreparedStatement statement = conexion.prepareStatement(sql)) {
+
+         statement.setString(1, carnet);
+
+         try (ResultSet fila = statement.executeQuery()) {
+             while (fila.next()) {
+                 int id = fila.getInt("id");
+                 String nombre = fila.getString("nombre");
+                 int creditos = fila.getInt("creditos");
+                 resultado.add(new Curso(id, nombre, creditos));
+             }
+         }
+     }
         return resultado;
     }
 
